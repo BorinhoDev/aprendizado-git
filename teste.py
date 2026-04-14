69480dcf3b4909b557f5dce498900c9f5cf38c0d
Gabriel Sanches <171sanches157@gmail.com>
2026-04-14 18:31:26 -0300

    Features: Adição de idade
    Adição da variavel age dentro da tabela idade, porem nao esta funcionando corretamente.

--- teste.py
+++ teste.py
@@ -1,8 +1,11 @@
 nomes = []
+idade = []
 
 for i in range(2):
     name = str(input("Digite o nome do aluno: "))
     nomes.append(name)
+    age = int(input("Digite a idade do aluno: "))
+    idade.append(age)
 
 for name in nomes:
-    print(name)
+    print(f"O aluno {name} tem {age} anos.")
